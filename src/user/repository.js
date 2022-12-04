457398f244bbe6c9dffd910d8480e91c9c58671e
Gil Campini <campinigil@gmail.com>
2022-12-04 15:39:00 +0100

    fix: user repo

--- src/user/repository.js
+++ src/user/repository.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
-const USERS_FILE_PATH = __dirname + '/../../data/users.json';
+const DATA_PATH = __dirname + '/../../data';
+const USERS_FILE_PATH = DATA_PATH + '/users.json';
 save(true);
 const users = require(USERS_FILE_PATH);
 const crypto = require("crypto");
@@ -10,9 +11,11 @@ const {UserDoesNotExistError} = require("./errors");
  * Saves the users content to the file.
  */
 function save(init) {
+    if (!fs.existsSync(DATA_PATH)) {
+        fs.mkdirSync(DATA_PATH);
+    }
     if (!fs.existsSync(USERS_FILE_PATH)) {
-        fs.mkdirSync(__dirname + '/../../data');
-        fs.writeFileSync(USERS_FILE_PATH, '[]');
+        fs.writeFileSync(USERS_FILE_PATH, JSON.stringify([]));
     }
     if (!init) {
         fs.writeFileSync(USERS_FILE_PATH, JSON.stringify(users, null, 2));
